2869fc04ff5061db938e2e118d502f2d2dbd661e
ztischenko <zoryana.tischenko@gmail.com>
2017-06-02 14:58:56 +0200

    Added i18n support and language files for signup/login pages in 8 languages. Added and created css/js for dropdown with languages in signup page. Fixed issue with button being not aligned strictly right on various screens.

--- client/templates/sign_in/sign_in.js
+++ client/templates/sign_in/sign_in.js
@@ -1,6 +1,45 @@
 Template.sign_in.onRendered(function(){
-		 $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+	/*var d = new Date();
+d.setFullYear( d.getFullYear() - 100 );
+$('.datepicker').pickadate(
+{
+   selectMonths: true,
+   selectYears: d,
+   max: new Date() //TODO- research documentaton
+});*/
+		$('.datepicker').pickadate({
+		    selectMonths: true, // Creates a dropdown to control month
+		    selectYears: 45 // Creates a dropdown of 15 years to control year
+		  });
+		 $('select').material_select();
+		 $('ul.dropdown-content.select-dropdown li').click(function(e){
+		 	$('label.i18n-select').addClass("active");
+		 	if(e.target.innerText.length > 0){
+		 		var langs = {
+		 			"العربية":"ar",
+		 			"English":"en",
+		 			"Deutsch":"de",
+		 			"Français":"fr",
+		 			"Polski":"pl",
+		 			"Русский":"ru",
+		 			"Svenska":"sv",
+		 			"Українська":"uk"
+		 		};
+				TAPi18n.setLanguage(langs[e.target.innerText.trim()])
+			      .done(function () {
+			      	//TODO:store user language
+			      	console.log("Language was set " + TAPi18n.getLanguage());
+			        //Session.set("showLoadingIndicator", false);
+			      })
+			      .fail(function (error_message) {
+			        // Handle the situation
+			        console.log(error_message);
+			      });
+			}
+		 	
+		 });
+});
+
+Template.sign_in.events({
+
 });
