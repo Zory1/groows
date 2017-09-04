478a60396c84564fe5026915618dc87deff7f5e9
ztischenko <zoryana.tischenko@gmail.com>
2017-09-04 13:47:08 +0200

    timeline: added social emoji and action icons.

--- client/templates/timeline/timeline.js
+++ client/templates/timeline/timeline.js
@@ -3,3 +3,12 @@ Template.timeline_tabs.onRendered(function(){
 		//debugger;
 		$('ul.tabs').tabs({ 'swipeable': true });
 });
+
+ Template.timeline_content.onRendered(function(){
+    console.log("Entered timeline_content on render");
+});
+
+Template.timeline_content.helpers({
+	tags: function(){return  ["winter","snow","park","walk with a dog","good day","colors","smells","emotions"]}
+	//"content_tags": function(){ console.log("inside timeline-content helper"); return ["winter","snow","park","walk with a dog","good day","colors","smells","emotions"]}
+});
